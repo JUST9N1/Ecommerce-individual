c7a741ed6bda379e06ebdef038561c938278111d
Sujan Gautam <susangautam062@gmail.com>
2023-07-27 01:05:19 +0545

    some code modified in homescreen.

--- lib/widgets_common/home_buttons.dart
+++ lib/widgets_common/home_buttons.dart
@@ -1,13 +1,12 @@
 import 'package:emart_app/consts/consts.dart';
 
-Widget homeButtons() {
-  return Expanded(
-      child: Column(
+Widget homeButtons({width, height, icon, String? title, onPress}) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
     children: [
-  Image.asset(icTodaysDeal, width: 26),
-  5.heightBox,
-  todayDeal.text.fontFamily(semibold).color(darkFontGrey).make()
+      Image.asset(icon, width: 26),
+      10.heightBox,
+      title!.text.fontFamily(semibold).color(darkFontGrey).make()
     ],
-  )
-  ).box.rounded.white.make();
+  ).box.rounded.white.size(width, height).make();
 }
