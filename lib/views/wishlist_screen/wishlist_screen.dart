e1e5f091809ee74211cd7ff40ff75c3338274c9e
Sujan Gautam <susangautam062@gmail.com>
2023-07-29 17:46:51 +0545

    wishlist created dynamically

--- lib/views/wishlist_screen/wishlist_screen.dart
+++ lib/views/wishlist_screen/wishlist_screen.dart
@@ -11,7 +11,8 @@ class WishlistScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: whiteColor,
       appBar: AppBar(
-        title: "My Wishlist".text.color(darkFontGrey).fontFamily(semibold).make(),
+        title:
+            "My Wishlist".text.color(darkFontGrey).fontFamily(semibold).make(),
       ),
       body: StreamBuilder(
         stream: FirestoreServices.getWishlists(),
@@ -24,7 +25,49 @@ class WishlistScreen extends StatelessWidget {
                 .color(darkFontGrey)
                 .makeCentered();
           } else {
-            return Container();
+            var data = snapshot.data!.docs;
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        leading: Image.network(
+                          '${data[index]['p_imgs'][0]}',
+                          width: 80,
+                          fit: BoxFit.cover,
+                        ),
+                        title: "${data[index]['p_name']}"
+                            .text
+                            .fontFamily(semibold)
+                            .size(16)
+                            .make(),
+                        subtitle: "${data[index]['p_price']}"
+                            .numCurrency
+                            .text
+                            .color(redColor)
+                            .fontFamily(semibold)
+                            .make(),
+                        trailing: Icon(
+                          Icons.favorite,
+                          color: redColor,
+                        ).onTap(() async {
+                          await firestore
+                              .collection(productsCollection)
+                              .doc(data[index].id)
+                              .set({
+                            'p_wishlist':
+                                FieldValue.arrayRemove([currentUser!.uid])
+                          }, SetOptions(merge: true));
+                        }),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
           }
         },
       ),
